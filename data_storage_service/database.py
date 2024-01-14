ba7578fb016129cb2d5ff9286a635e108e649eff
Nata Butrina <hatecodinglovemoney@gmail.com>
2024-01-14 23:45:48 +0300

    fix: fixed db connection and added history

--- data_storage_service/database.py
+++ data_storage_service/database.py
@@ -3,18 +3,31 @@ import os
 from pymongo import MongoClient
 from pymongo import ReturnDocument
 
-client = MongoClient("mongodb://mongo:27017/")  # Посмотреть, где будет.
+
+host = "mongo"
+port = 27017
+username = os.getenv("MONGO_INITDB_ROOT_USERNAME", "username")
+password = os.getenv("MONGO_INITDB_ROOT_PASSWORD", "password")
+
+client = MongoClient(f"mongodb://{username}:{password}@{host}:{port}/")
 db_name = os.getenv("MONGO_INITDB_DATABASE", "default_database")
 db = client[db_name]
 
 
 def record_data(hashed_identifier: str, data: dict, api_url: str):
     collection = db[api_url]
-    # Решить с историей изменений.
     existing_record = collection.find_one_and_update(
         {"_id": hashed_identifier},
         {"$set": data},
-        return_document=ReturnDocument.BEFORE,  # Можно решить с историей изменений с помощью этого (?)
-        upsert=True  # Создаст новый документ, если не найдет.
+        return_document=ReturnDocument.BEFORE,
+        upsert=True,
     )
-    pass
+    if existing_record:
+        history_collection = db["history_" + api_url]
+        history_collection.insert_one(existing_record)
+
+
+def get_data(url: str):  # TODO: Удалить это
+    collection = db[url]
+    documents = collection.find()
+    return [doc for doc in documents]
--- data_storage_service/routers.py
+++ data_storage_service/routers.py
@@ -1,6 +1,7 @@
 import requests
 from fastapi import FastAPI
 
+from data_storage_service.database import get_data
 from data_storage_service.database import record_data
 from data_storage_service.utils.exceptions import ApiRequestError
 from data_storage_service.utils.helpers import hash_data
@@ -9,21 +10,22 @@ from data_storage_service.utils.schemas import ApiRequestSchema
 app = FastAPI()
 
 
-@app.post("/get_and_record_data")
+@app.post("/record_data")
 def get_and_record_data(request_data: ApiRequestSchema) -> None:
     response = requests.get(request_data.api_url)
     if response.status_code != 200:
-        raise ApiRequestError()
-        # Или retry (добавить проверку на наличие флага в request_data).
+        raise ApiRequestError(f"API response status code: {response.status_code}")
+        # Или retry (тогда добавить проверку на наличие флага в request_data).
     response_body = response.json()
     unique_values = {key: response_body[key] for key in request_data.unique_combination}
     hashed_identifier = hash_data(data=unique_values)
     record_data(
         hashed_identifier=hashed_identifier,
         data=response_body,
-        api_url=request_data.api_url
+        api_url=request_data.api_url,
     )
-    return None
 
 
-
+@app.get("/get_data")
+def get_data_by_url(url: str):  # TODO: Удалить это
+    return get_data(url=url)
